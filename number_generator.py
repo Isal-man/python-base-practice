344ecffa68c207ad5de2c7d613b98e0969b39104
ahmad.faisal <faiskweren7@gmail.com>
2024-04-05 11:17:25 +0700

    add new generator with py

---
+++ number_generator.py
@@ -0,0 +1,4 @@
+import random
+
+print("imo:", random.randint(10000,99999))
+print("mmsi:", random.randint(100000000,999999999))
---
+++ uuid_generator.py
@@ -0,0 +1,6 @@
+import uuid
+
+# for x in range(1):
+#     print(str(uuid.uuid4()))
+
+print(str(uuid.uuid4()))
